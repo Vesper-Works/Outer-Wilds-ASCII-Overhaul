0c8ccf87e67513200737cdcd5805829a2f345b7e
Vesper <0ooechooo0oooo@gmail.com>
2021-10-08 21:48:06 +0100

    Switched filenames

--- ModTemplate/StarterBehaviour.cs
+++ ModTemplate/StarterBehaviour.cs
@@ -1,72 +1,31 @@
 ﻿using OWML.ModHelper;
+using OWML.Common;
+using UnityEngine;
 using OWML.ModHelper.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
 
 namespace OuterWildsxASCII
 {
-    class ShaderApplier : ModBehaviour
+    public class StarterBehaviour : ModBehaviour
     {
-        RenderTexture cameraTexture;
-        RenderTexture asciiTexture;
-        ComputeShader computeShader;
-        bool running = false;
-
         private void Start()
         {
 
-            ModHelper.Console.WriteLine("Creating textures...");
-            asciiTexture = new RenderTexture(1920, 1080, 0);
-            asciiTexture.enableRandomWrite = true;
-            asciiTexture.filterMode = FilterMode.Point;
-            asciiTexture.Create();
-
-            cameraTexture = new RenderTexture(1920, 1080, 0);
-            cameraTexture.enableRandomWrite = true;
-            cameraTexture.filterMode = FilterMode.Point;
-            cameraTexture.Create();
-
+            ModHelper.Console.WriteLine("Skipping splash screen...");
+            var titleScreenAnimation = FindObjectOfType<TitleScreenAnimation>();
+            titleScreenAnimation.SetValue("_fadeDuration", 0);
+            titleScreenAnimation.SetValue("_gamepadSplash", false);
+            titleScreenAnimation.SetValue("_introPan", false);
+            titleScreenAnimation.Invoke("FadeInTitleLogo");
             ModHelper.Console.WriteLine("Done!");
 
-            ModHelper.Console.WriteLine("Loading bundle...");
+            ModHelper.Events.Scenes.OnCompleteSceneChange += OnCompleteSceneChange;
 
-            var shaderbundle = ModHelper.Assets.LoadBundle("asciishader");
-
-            ModHelper.Console.WriteLine("Loading compute shader...");
-
-            computeShader = shaderbundle.LoadAsset<ComputeShader>("ASCIIShader");
-
-            ModHelper.Console.WriteLine("Done!");
-            ModHelper.Console.WriteLine("Getting cameras...");
-            ModHelper.Console.WriteLine("Instantiating texture cam");
-            GameObject textureCameraGO = new GameObject();
-            Camera camera = textureCameraGO.AddComponent<Camera>();
-            ModHelper.Console.WriteLine("Setting parent");
-            textureCameraGO.transform.SetParent(transform);
-            ModHelper.Console.WriteLine("Setting target texture");
-            camera.targetTexture = cameraTexture;
-            running = true;
-            ModHelper.Console.WriteLine("Done!");
         }
-
-
-        void Update()
+        private void OnCompleteSceneChange(OWScene oldScene, OWScene newScene)
         {
-            if (!running) { return; }
-            computeShader.SetTexture(0, "cameraTexture", cameraTexture);
-            computeShader.SetTexture(0, "asciiTexture", asciiTexture);
-            computeShader.SetFloat("scaleFactor", 1);
-            computeShader.Dispatch(0, 1920 / (8 * 8), 1080 / (8 * 8), 1);
+            if (newScene != OWScene.SolarSystem) { return; }
+            Camera.main.gameObject.AddComponent<ShaderApplier>();
         }
-
-        private void OnRenderImage(RenderTexture source, RenderTexture destination)
-        {
-            if (!running) { return; }
-            Graphics.Blit(asciiTexture, destination);
-        }
-    
     }
 }
